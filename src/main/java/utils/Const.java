e5ae09909eede8121564cc6b86554a34efe8354f
TooAndy <aniss@qq.com>
2018-09-29 18:30:05 +0800

    update

--- src/main/java/utils/Const.java
+++ src/main/java/utils/Const.java
@@ -5,6 +5,6 @@ package utils;
  * create time: 2018/9/29 16:50
  */
 public class Const {
-    public static String  ZK_QUORUM = ConfigManager.getProperty("zookeeper.quorum");
+    public static String ZK_QUORUM = ConfigManager.getProperty("zookeeper.quorum");
     public static String ZK_PORT = ConfigManager.getProperty("zookeeper.port");
 }
